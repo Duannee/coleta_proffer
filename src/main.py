fdf6733c757525abc7bd45df63b711afa433b5ba
Duanne Moraes <duanne.souza@gmail.com>
2025-03-04 21:29:09 -0300

    feat: Create search product function

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import json
 import logging
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
-logger = logging.basicConfig(
+logging.basicConfig(
     filename="scraping.log",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
@@ -36,3 +40,42 @@ class Scraper:
 
     def start_driver(self):
         return webdriver.Chrome(service=self.service, options=self.chrome_options)
+
+    def search_product(self, ean):
+        driver = self.start_driver
+        url = "https://precodahora.ba.gov.br/"
+        driver.get(url)
+        time.sleep(2)
+
+        try:
+            search_box = driver.find_element(By.XPATH, "//input[@id='fake-sbar']")
+            search_box.clear()
+            search_box.send_keys(ean)
+            search_box.send_keys(Keys.RETURN)
+            time.sleep(3)
+
+            description = driver.find_element(
+                By.XPATH, "//div[@style='font-size:18px;']/strong"
+            ).text
+            price = driver.find_element(By.XPATH, "//span[@class='search-gtin']").text
+            collection_date = driver.find_element(
+                By.XPATH, "//i[@class='fa fa-calendar']"
+            ).text
+            establishment = driver.find_element(
+                By.XPATH, "//i[@class='fa fa-building']"
+            ).text
+
+            data = {
+                "ean": ean,
+                "description": description,
+                "price": price,
+                "collection_date": collection_date,
+                "establishment": establishment,
+            }
+            logging.info(f"Success: {ean} - {description} - {price}")
+            return data
+        except (NoSuchElementException, TimeoutException) as e:
+            logging.error(f"Error fetching {ean}: {e}")
+            return None
+        finally:
+            driver.quit()
