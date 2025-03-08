f4bbbdb3c9d13c2c1d299cdd2118c0d06f1d7a84
Duanne Moraes <duanne.souza@gmail.com>
2025-03-07 22:14:50 -0300

    refactor: Change logic in solve_recaptcha method

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import logging
 import time
 import requests
 import pandas as pd
+import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
@@ -16,6 +17,8 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from stale_element import WebElementWrapper
+from anticaptchaofficial.recaptchav2proxyless import *
+
 
 logging.basicConfig(
     filename="scraping.log",
@@ -39,7 +42,7 @@ class Scraper:
 
     def _initialize_driver(self):
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         service = Service(ChromeDriverManager().install())
@@ -106,48 +109,60 @@ class Scraper:
 
     def solve_recaptcha(self):
         try:
-            recaptcha_iframes = self.driver.find_element(
-                By.XPATH, "//iframe[@title='reCAPTCHA']"
-            )
-            if not recaptcha_iframes:
-                print("No reCAPTCHA found, following normal flow...")
-                return
+            challenge_url = "https://precodahora.ba.gov.br/challenge/"
+            self.driver.get(challenge_url)
+            time.sleep(3)
 
-            recaptcha_iframe = recaptcha_iframes[0]
-            self.driver.switch_to.frame(recaptcha_iframe)
-
-            checkbox = WebDriverWait(self.driver, 10).until(
-                EC.element_to_be_clickable(
-                    (By.XPATH, "//div[@class='recaptcha-checkbox-border']")
-                )
-            )
-            checkbox.click()
-            print("Clicked on reCAPTCHA")
-
-            self.driver.switch_to.default_content()
-
-            WebDriverWait(self.driver, 5).until(
+            WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located(
-                    (By.XPATH, "//div[@class='recaptcha-checkbox-checkmark']")
+                    (By.XPATH, "//div[@class='g-recaptcha']")
                 )
             )
-            print("reCAPTCHA solved")
-            try:
-                submit_button = WebDriverWait(self.driver, 5).until(
-                    EC.element_to_be_clickable(
-                        (By.XPATH, "//button[contains(text(), 'Enviar')]")
+            captcha_key = self.driver.find_element(
+                By.XPATH, "//div[@class='g-recaptcha']"
+            ).get_attribute("data-sitekey")
+
+            print(f"Captcha Site Key: {captcha_key}")
+
+            if not captcha_key:
+                raise ValueError("Failed to extract captcha site key")
+
+            solver = recaptchaV2Proxyless()
+            solver.set_verbose(1)
+            api_key = "336489876b0bbce2d55cdfb7f330c134"
+            if not api_key:
+                raise ValueError(
+                    "AntiCaptcha API key is not defined in the environment"
+                )
+            solver.set_key(api_key)
+            solver.set_website_url(challenge_url)
+            solver.set_website_key(captcha_key)
+
+            response = solver.solve_and_return_solution()
+            print(f"Captcha Response: {response}")
+
+            if response != 0:
+                WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((By.ID, "g-recaptcha-response"))
+                )
+                self.driver.execute_script(
+                    "document.getElementById('g-recaptcha-response').value = arguments[0];"
+                    "document.getElementById('g-recaptcha-response').dispatchEvent(new Event('change'));",
+                    response,
+                )
+                time.sleep(5)
+                try:
+                    submit_button = self.driver.find_element(
+                        By.CSS_SELECTOR, ".btn.btn-lg.btn-danger.mt-2.btn-block"
                     )
-                )
-                submit_button.click()
-                print("Clicked the submit button after reCAPTCHA!")
-            except TimeoutException:
-                print(
-                    "Submit button did not appear even after solving the reCAPTCHA, proceeding normally..."
-                )
-        except (TimeoutException, NoSuchElementException):
-            print(
-                "No reCAPTCHA detected or timeout when trying to solve it, following the normal flow..."
-            )
+                    submit_button.click()
+                    time.sleep(5)
+                except NoSuchElementException:
+                    print("CAPTCHA button not found")
+            else:
+                print(f"Captcha solving failed: {solver.err_string}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def search_product(self, ean, city_code):
         self.driver.get(self.base_url)
