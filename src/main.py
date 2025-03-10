033d34495f2baf86ba47a4beeb0446496b57e181
Duanne Moraes <duanne.souza@gmail.com>
2025-03-09 23:26:02 -0300

    feat: Create others USER_AGENTS

--- src/main.py
+++ src/main.py
@@ -27,6 +27,12 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+]
+
 
 class Scraper:
     def __init__(self):
