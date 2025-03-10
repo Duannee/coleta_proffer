285f129407846668c6bc605c50efdf8936a7218e
Duanne Moraes <duanne.souza@gmail.com>
2025-03-10 07:39:01 -0300

    feat: Change logic for the ThreadPoolExecutor

--- src/main.py
+++ src/main.py
@@ -21,6 +21,7 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from stale_element import WebElementWrapper
+from itertools import zip_longest
 
 
 logging.basicConfig(
@@ -32,7 +33,6 @@ logging.basicConfig(
 
 class Scraper:
     def __init__(self):
-        self.driver = self._initialize_driver()
         self.base_url = "https://precodahora.ba.gov.br/produtos/"
         self.cities = {"Salvador": "2927408", "Feira de Santana": "2910800"}
         self.state_code = "29"
@@ -102,9 +102,9 @@ class Scraper:
             print(f"Connection error when querying CNPJ {cnpj}: {e}")
             return None
 
-    def extract_cnpj(self):
+    def extract_cnpj(self, driver):
         try:
-            cnpj_element = WebDriverWait(self.driver, 10).until(
+            cnpj_element = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, "//button[@id='hbtn_1-0']"))
             )
             return cnpj_element.get_attribute("data-cnpj")
@@ -113,17 +113,16 @@ class Scraper:
             print("CNPJ not found")
             return None
 
-    def solve_recaptcha(self):
-
+    def solve_recaptcha(self, driver):
         sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
         challenge_url = "https://precodahora.ba.gov.br/challenge/"
-        self.driver.get(challenge_url)
+        driver.get(challenge_url)
         time.sleep(3)
 
-        WebDriverWait(self.driver, 10).until(
+        WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//div[@class='g-recaptcha']"))
         )
-        captcha_key = self.driver.find_element(
+        captcha_key = driver.find_element(
             By.XPATH, "//div[@class='g-recaptcha']"
         ).get_attribute("data-sitekey")
 
@@ -142,59 +141,57 @@ class Scraper:
             code = result["code"]
             print(code)
 
-            WebDriverWait(self.driver, 10).until(
+            WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.ID, "g-recaptcha-response"))
             )
 
-            self.driver.execute_script(
+            driver.execute_script(
                 f"document.getElementById('g-recaptcha-response').innerHTML = '{code}';"
             )
 
-            self.driver.execute_script(
+            driver.execute_script(
                 "var event = new Event('input', { bubbles: true });"
                 "document.getElementById('g-recaptcha-response').dispatchEvent(event);"
             )
 
-            self.driver.execute_script(
+            driver.execute_script(
                 "var event = new Event('change', { bubbles: true });"
                 "document.getElementById('g-recaptcha-response').dispatchEvent(event);"
             )
 
             time.sleep(2)
 
-            self.driver.find_element(
+            driver.find_element(
                 By.CSS_SELECTOR, ".btn.btn-lg.btn-danger.mt-2.btn-block"
             ).click()
 
-            WebDriverWait(self.driver, 10).until(EC.url_changes(challenge_url))
+            WebDriverWait(driver, 10).until(EC.url_changes(challenge_url))
             print("CAPTCHA solved and page processed successfully!!")
 
         except Exception as e:
             print(f"Erro ao resolver CAPTCHA: {e}")
 
-    def search_product(self, ean, city_code):
-        self.driver.get(self.base_url)
+    def search_product(self, driver, ean, city_code):
+        driver.get(self.base_url)
         time.sleep(2)
 
-    def extract_product_data(self, ean, description, city_code):
+    def extract_product_data(self, driver, ean, description, city_code):
         try:
-            self.driver.get(self.base_url)
+            driver.get(self.base_url)
             time.sleep(random.uniform(1.5, 3))
-            search_box = WebElementWrapper(
-                self.driver, By.XPATH, "//input[@id='top-sbar']"
-            )
+            search_box = WebElementWrapper(driver, By.XPATH, "//input[@id='top-sbar']")
             search_box.get_element().clear()
             search_box.send_keys(ean)
             search_box.send_keys(Keys.RETURN)
             time.sleep(3)
 
-            self.solve_recaptcha()
+            self.solve_recaptcha(driver)
 
             with open("page_source.html", "w", encoding="utf-8") as f:
-                f.write(self.driver.page_source)
-            self.driver.save_screenshot("screenshot.png")
+                f.write(driver.page_source)
+            driver.save_screenshot("screenshot.png")
 
-            no_results = self.driver.find_elements(
+            no_results = driver.find_elements(
                 By.XPATH, "//div[contains(text(), 'No results found')]"
             )
             if no_results:
@@ -202,15 +199,15 @@ class Scraper:
                 return None
 
             price = WebElementWrapper(
-                self.driver,
+                driver,
                 By.XPATH,
                 "//div[@style='font-size:42px;font-weight:bold; color: #000;']",
             ).text()
             establishment = WebElementWrapper(
-                self.driver, By.XPATH, "//div[i[@class='fa fa-building']]"
+                driver, By.XPATH, "//div[i[@class='fa fa-building']]"
             ).text()
 
-            cnpj = self.extract_cnpj()
+            cnpj = self.extract_cnpj(driver)
             if not cnpj:
                 print(f"No CNPJ found for EAN {ean}, skipping query.")
 
@@ -241,27 +238,36 @@ class Scraper:
             return data
         except TimeoutException:
             print("Timed out while trying to click the submit button!")
+            return None
         except NoSuchElementException:
             print(f"Product with EAN {ean} not found in city {city_code}.")
             return None
-        except TimeoutException:
-            print(
-                f"Timeout while searching for product with EAN {ean} in city {city_code}."
-            )
-            return None
 
     def collect_data_for_city(self, ean, description, city_name, city_code):
-        print(f"Fetching data for EAN {ean} in {city_name}")
-        self.search_product(ean, city_code)
-        product_data = self.extract_product_data(ean, description, city_code)
-        if product_data:
-            return product_data
-        return None
+        driver = self._initialize_driver()
+        try:
+            print(f"Fetching data for EAN {ean} in {city_name}")
+            self.search_product(driver, ean, city_code)
+            product_data = self.extract_product_data(
+                driver, ean, description, city_code
+            )
+            if product_data:
+                return product_data
+            return None
+        finally:
+            driver.quit()
 
     def collect_data(self, ean_list, description_list):
         with ThreadPoolExecutor(max_workers=5) as executor:
             futures = []
-            for ean, description in zip(ean_list, description_list):
+            for ean, description in zip_longest(
+                ean_list, description_list, fillvalue=None
+            ):
+                if ean is None or description is None:
+                    logging.warning(
+                        "Skipping incomplete pair: EAN or description is missing"
+                    )
+                    continue
                 for city_name, city_code in self.cities.items():
                     print(f"Fetching data for EAN {ean} in {city_name}")
                     futures.append(
@@ -273,19 +279,17 @@ class Scraper:
                             city_code,
                         )
                     )
-                for future in as_completed(futures):
-                    result = future.result()
-                    if result:
-                        self.results.append(result)
+
+            for future in as_completed(futures):
+                result = future.result()
+                if result:
+                    self.results.append(result)
         print(f"Total results collected: {len(self.results)}")
 
     def save_csv(self, file_name="data_collected.csv"):
         df = pd.DataFrame(self.results)
         df.to_csv(file_name, index=False, encoding="utf-8")
         print(f"Data saved to {file_name}")
-
-    def close_driver(self):
-        self.driver.quit()
 
 
 if __name__ == "__main__":
@@ -298,5 +302,5 @@ if __name__ == "__main__":
         scraper.collect_data(ean_list, description_list)
 
         scraper.save_csv("data_collected.csv")
-    finally:
-        scraper.close_driver()
+    except Exception as e:
+        print(f"An error occurred: {e}")
